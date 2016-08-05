13317de04b2b870dc2c91f805976e687f4682c86
Daniel He <Daniel He>
2016-08-05 15:24:53 -0700

    init with fixed gitignore

---
+++ Assets/Scripts/UpScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpScript : MonoBehaviour {
+
+    public Vector2 end_pos;
+    public float moveSpeed = 10;
+    public float stopSpeed = 0;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 cur_pos = transform.position;
+        moveSpeed = end_pos.y - cur_pos.y + 0.1f;
+
+        if ( cur_pos.y < end_pos.y)
+        {
+            rb.velocity = new Vector2(0, moveSpeed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, stopSpeed);
+        }
+    }
+}
