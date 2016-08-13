590b36933b22d0df77ed3ebb241cf18d5d6cd147
Peachball <chen_xu@live.com>
2016-08-12 20:59:21 -0700

    Added json library to project, for future storage of dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject textbox;
+    /*
+        The dialogue will be formatted as a json list representing the speech of two people
+
+        e.g.
+        [
+        {
+            name : 'name of person talking',
+            text : 'what person is saying',
+            time : 'how long it takes for the text to be rendered to the screen'
+        }
+        ]
+    */
+    private string dialogue = @"
+[
+{
+    name : 'A',
+    text : 'HIIIIII',
+    time : 10
+},
+{
+    name : 'B',
+    text : 'BYEEEEEE',
+    time : 10
+}
+]
+";
+
+    private float start_time;
+    private JSONNode parsed_dialogue;
+
+	// Use this for initialization
+	void Start () {
+        start_time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void playDialogue()
+    {
+
+    }
+}
