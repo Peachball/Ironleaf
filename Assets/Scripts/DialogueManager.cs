1128d15c501144f4d991c33672e686779111debe
Peachball <chen_xu@live.com>
2016-08-13 12:32:39 -0700

    Dialogue works somewhat now (name not mentioned)

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using SimpleJSON;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
 
     public GameObject textbox;
+    private Text t;
+
     /*
         The dialogue will be formatted as a json list representing the speech of two people
 
@@ -17,16 +20,18 @@ public class DialogueManager : MonoBehaviour {
         }
         ]
     */
+    
+    //Placeholder (dialogue should be loaded in dynamically from txt files later
     private string dialogue = @"
 [
 {
     name : 'A',
-    text : 'HIIIIII',
-    time : 10
+    text : HIIIIII,
+    time : 1
 },
 {
     name : 'B',
-    text : 'BYEEEEEE',
+    text : BYEEEEEE,
     time : 10
 }
 ]
@@ -34,19 +39,24 @@ public class DialogueManager : MonoBehaviour {
 
     private float start_time;
     private JSONNode parsed_dialogue;
+    private int line_num = 0;
 
 	// Use this for initialization
 	void Start () {
         start_time = Time.time;
+        parsed_dialogue = JSON.Parse(dialogue);
+        t = textbox.GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float max_elapsed_time = parsed_dialogue[line_num]["time"].AsFloat;
+        if (Time.time - start_time > max_elapsed_time) {
+            //Change text of textbox to reflect dialogue
+            t.text = parsed_dialogue[line_num]["text"];
+
+            line_num++;
+            start_time = Time.time;
+        }
 	}
-
-    public void playDialogue()
-    {
-
-    }
 }
