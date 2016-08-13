33f52a94843cb9d447a9382a017ce114d3bce535
Peachball <chen_xu@live.com>
2016-08-13 13:08:13 -0700

    Character control added to game

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(0, 0);
+        if (Input.GetButton("Up"))
+        {
+            rb.velocity = new Vector2(0, speed);
+        }
+        if (Input.GetButton("Down"))
+        {
+            rb.velocity = new Vector2(0, -speed);
+        }
+        if (Input.GetButton("Right"))
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        if (Input.GetButton("Left"))
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+	}
+}
