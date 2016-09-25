c266b7037ad75219a0b7af78c23c7ce805de7cff
Peachball <chen_xu@live.com>
2016-09-24 22:45:14 -0700

    Added functional door scripts

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -4,13 +4,40 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
+    public GameObject startscene = null;
+
     public void onclick(string scene)
     {
         SceneManager.LoadScene(scene);
     }
+
+    public void changeroom(string scene)
+    {
+        GameObject[] rooms = GameObject.FindGameObjectsWithTag("room");
+        GameObject g = null;
+        foreach(GameObject r in rooms)
+        {
+            if(r.name == scene)
+            {
+                g = r;
+            }
+        }
+        if(g != null)
+        {
+            if (startscene != null)
+            {
+                startscene.transform.position = new Vector3(100, 100, -20);
+            }
+            g.transform.position = new Vector3(0, 0, 0);
+            startscene = g;
+        }
+    }
 	// Use this for initialization
 	void Start () {
-	
+	    if(startscene != null)
+        {
+            startscene.transform.position = new Vector3(0, 0, 0);
+        }
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorManager : MonoBehaviour {
+
+    public string nextscene;
+    public ChangeScene scenemanager;
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        scenemanager.changeroom(nextscene);
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
