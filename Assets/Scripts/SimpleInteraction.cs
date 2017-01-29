4cb9d04bd406a0a8363a9a6fb6e54e57bff10cad
Peachball <chenyexu@gmail.com>
2017-01-29 14:39:56 -0800

    Added crown and replaced flowermanager script
    Now, simpleinteractionscript will be the component used whenever adding an
    object that shows a message when interacted with

---
+++ Assets/Scripts/SimpleInteraction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleInteraction : MonoBehaviour {
+
+    public string message;
+    private DialogueManager dbox;
+
+	// Use this for initialization
+	void Start () {
+        GameObject d = GameObject.Find("DialogueBackground");
+        dbox = d.GetComponent<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D c){
+		if(c.gameObject.name == "Main Character"
+				&& Input.GetButton("Interact")){
+            dbox.setText(message);
+			dbox.show();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D c){
+		if(c.gameObject.name == "Main Character"){
+			dbox.setText("");
+			dbox.hide();
+		}
+	}
+}
